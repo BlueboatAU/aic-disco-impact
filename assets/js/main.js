7ed29bee2d448e85f21dd6e945838db458379e09
Guy Aldous <guy@blueboat.com.au>
2019-06-04 17:32:49 +1000

    homepage mostly complete with responive lines and arrows

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,15 +17,65 @@ function drawLine(d1, d2, l){
   let line = $(`#${l}`);
   let pos1 = div1.offset();
   let pos2 = div2.offset();
+
   line.attr('x1', pos1.left).attr('y1', pos1.top).attr('x2', pos2.left).attr('y2', pos2.top);
 
-  console.log(`${pos1.top} ${pos1.left} and ${pos2.top}`);
+  //used for testing ==> console.log(`${pos1.top} ${pos1.left} and ${pos2.top}`);
+}
+
+function angle(cx, cy, ex, ey) {
+  var dy = ey - cy;
+  var dx = ex - cx;
+  var theta = Math.atan2(dy, dx); // range (-PI, PI]
+  theta *= 180 / Math.PI; // rads to degs, range (-180, 180]
+  //if (theta < 0) theta = 360 + theta; // range [0, 360)
+  return theta;
+}
+
+//function that draws the circles
+function drawCircle(d1, d2, c, a){
+  let div1 = $(`#${d1}`);
+  let div2 = $(`#${d2}`);
+  let circle = $(`#${c}`);
+  let arrow = $(`#${a}`);
+  let arrowpoly = $(`#${a} polyline`);
+  let pos1 = div1.offset();
+  let pos2 = div2.offset();
+  let midx = pos1.left + (pos2.left - pos1.left)/2;
+  let midy = pos1.top + (pos2.top - pos1.top)/2;
+  let ang = angle(pos1.left, pos1.top, pos2.left, pos2.top) + 235
+
+  circle.attr('cx', midx).attr('cy', midy);
+  arrow.attr('x', midx-4.5).attr('y', midy-4)
+  arrowpoly.attr('transform', `rotate(${ang.toFixed(0)}, 5 , 5)`);
+
+  console.log(`${midx} and ${midy} - ${pos1.left} and ${pos2.left}`);
+  console.log(ang);
+
 }
 
 //list of lines to draw
 function drawTheLines(){
+  let docsize = $('.body-sans-lines').height();
+  //used for testing ==> console.log(docsize);
+  $('.linesvg').css('height', docsize);
+  //about lines
   drawLine('desc-target', 'about-target', 'aboutline');
   drawLine('about-target-end', 'chair-target', 'chairline');
+  //res lines
+  drawLine('ene-target', 'inf-target', 'eneline');
+  drawLine('inf-target', 'urb-target', 'infline');
+  drawLine('urb-target', 'hea-target', 'urbline');
+  drawLine('hea-target', 'foo-target', 'healine');
+  //att and rel lines
+  drawLine('att-target', 'how-target', 'attline');
+  drawLine('rel-target', 'more-target', 'relline');
+  //draw circles
+  drawCircle('ene-target', 'inf-target', 'enecircle', 'enearrow');
+   drawCircle('inf-target', 'urb-target', 'infcircle', 'infarrow');
+  drawCircle('urb-target', 'hea-target', 'urbcircle', 'urbarrow');
+  drawCircle('hea-target', 'foo-target', 'heacircle', 'heaarrow');
+
 } 
 
 //init
